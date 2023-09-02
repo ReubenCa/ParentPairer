eca95653f5b966cccd11db42b8a4bc71cc862b03
Reuben Carolan <61869273+ReubenCa@users.noreply.github.com>
2023-09-02 11:41:14 +0100

    Add project files.

---
+++ ParentPairer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ParentPairer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ SimulatedAnnealing/ISolution.cs
@@ -0,0 +1,11 @@
+﻿namespace SimulatedAnnealing
+{
+    public interface ISolution
+    {
+        public double getScore();
+
+        public ISolution Mutate();
+
+        public ISolution Clone();
+    }
+}
